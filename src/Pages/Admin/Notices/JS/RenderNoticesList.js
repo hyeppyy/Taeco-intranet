844e14fbab125e5ed763b488af5a8beaf4b4f91a
hjkwon <sirru97@naver.com>
2024-07-02 16:15:22 +0900

    feat: 첨부파일 아이콘으로 변경 #25

--- src/Pages/Admin/Notices/JS/RenderNoticesList.js
+++ src/Pages/Admin/Notices/JS/RenderNoticesList.js
@@ -1,30 +1,53 @@
-import styles from "../../../Admin/Notices/Notice.module.css";
-
 const renderNoticesList = (data) => {
   const tableBody = document.querySelector("table > tbody");
 
   tableBody.innerHTML = ""; //초기화
 
-  data.forEach((data) => {
+  //table 구조 생성
+  data.forEach((dataItem) => {
     const row = document.createElement("tr");
+    const hasAttachments = dataItem.attachments.length > 0;
+
     row.innerHTML = `
-        <td id=${data.index}>${data.index}</td>
-        <td>${data.title}</td>
-        <td>${data.author}</td>
-        <td>${data.date}</td>
-        <td>${data.attachments}</td>
-        <td>${data.views}</td>
-        `;
+            <td >${dataItem.index}</td>
+            <td>${dataItem.title}</td>
+            <td>${dataItem.author}</td>
+            <td>${dataItem.date}</td>
+            <td>${
+              hasAttachments
+                ? `<img src="/public/icons/textfile.svg" alt="file-icon" width="20" height="20" />`
+                : ""
+            }</td>
+            <td>${dataItem.views}</td>
+      `;
+
+    // 각 행에 데이터 속성 할당
+    row.dataset.index = dataItem.index;
+    row.dataset.title = dataItem.title;
+    row.dataset.author = dataItem.author;
+    row.dataset.date = dataItem.date;
+    row.dataset.attachments = hasAttachments;
+    row.dataset.views = dataItem.views;
+    row.dataset.description = dataItem.description;
+
+    // 행 클릭 이벤트 리스너 추가
+    row.addEventListener("click", () => {
+      // 클릭한 행의 데이터를 URL 쿼리 파라미터로 변환
+      const noticeData = {
+        index: row.dataset.index,
+        title: row.dataset.title,
+        author: row.dataset.author,
+        date: row.dataset.date,
+        attachments: row.dataset.attachments,
+        views: row.dataset.views,
+        description: row.dataset.description,
+      };
+
+      const queryString = new URLSearchParams(noticeData).toString();
+      window.location.href = `/admin/notices/detail?${queryString}`;
+    });
+
     tableBody.appendChild(row);
-  });
-
-  //테이블 row 클릭시 상세페이지로 이동
-  const noticeTable = document.querySelector(`.${styles["adminNoticeTable"]}`);
-  noticeTable.addEventListener("click", (event) => {
-    window.location.href = "/admin/notices/detail";
-
-    // const row = event.target.closest("tr");
-    // console.log(row);
   });
 };
 
--- src/Pages/User/Notices/Detail/Detail.js
+++ src/Pages/User/Notices/Detail/Detail.js
@@ -1,28 +1,36 @@
-import detailInfo from "../JS/DetailInfo.js";
+import detailList from "../JS/DetailList.js";
 import styles from "../Detail/Detail.module.css";
 
 const renderUserNoticesDetail = (container) => {
   container.innerHTML = `
-     <h1 class="title">공지사항>상세</h1>
         <div class="${styles.noticeDetail}">
-        <h2>토스페이 결제 취소 / 환불이 필요해요. 결제토스페이환불</h2>
         <div class="${styles.noticeDetail__btnContainer}">
-        <button data-color="neutral" data-shape="line" class="${styles.noticeDetail__backBtn}">뒤로가기</button>
+          <button data-color="neutral" data-shape="line" class="${styles.noticeDetail__backBtn}">뒤로가기</button>
         </div>
-          <ul class="${styles.noticeDetail__info}"></ul>
-          <h4>
-            제품의 교환 및 반품, 환불은 모두 통신판매업자인 가맹점의 정책을
-            따르기 때문에 가맹점으로 문의가 필요해요. [가맹점 문의 앱 경로]
-            '토스페이' → '내 쇼핑' → '문의하기' → 원하는 결제 내역 선택 → '1:1
-            문의' 혹은 '전화 문의' [바로가기 링크] ※ 주말 및 공휴일은 가맹점에서
-            고객님의 문의 내용 확인이 어려울 수 있는 점 양해 부탁드려요. ※
-            신선식품의 경우 상품의 가치가 신선도에 영향을 받기 때문에 단순변심에
-            의한 교환 및 환불이 어려울 수 있어요
-          </h4>
+        <h2 class="${styles.noticeTitle}"></h2>
+          <ul class="${styles.noticeDetail__info}">
+            <li class=${styles.noticeDetail__items}>
+              <h5>등록일</h5>
+              <h5 class="${styles.registrationDate}"></h5>
+            </li>
+            <li class=${styles.noticeDetail__items}>
+              <h5>작성자</h5>
+              <h5 class="${styles.writer}"></h5>
+            </li>
+            <li class=${styles.noticeDetail__items}>
+              <h5>조회수</h5>
+              <h5 class="${styles.views}"></h5>
+            </li>
+            <li class=${styles.noticeDetail__items}>
+              <h5>첨부파일</h5>
+              <div class="${styles.attachedFile}"></div>
+            </li>
+          </ul>
+          <h4 class="${styles.noticeDetail__description}"></h4>
         </div>
   `;
-  detailInfo();
   moveBack();
+  detailList();
 };
 
 const moveBack = () => {

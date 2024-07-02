f216d5659e2092a80cda8c448631b17f65f40c23
Suhyun Park <suhyun_4951@naver.com>
2024-07-03 01:57:17 +0900

    feat: fetch 구현 및 페이지 css 수정

--- src/Pages/Admin/Employee/Employee.js
+++ src/Pages/Admin/Employee/Employee.js
@@ -1,6 +1,6 @@
 import styles from "./Employee.module.css";
 import renderAddEmployeePage from "./AddEmployee"; // 새로운 페이지 렌더링 함수 가져오기
-import employeeData from "./Employee.json";
+import { fetchEmployeeData } from "./FetchEmployeeData"; // fetch 함수 가져오기
 
 const renderAdminEmployee = (container) => {
   container.innerHTML = `
@@ -15,26 +15,25 @@ const renderAdminEmployee = (container) => {
 
   <div class="${styles.page__header}">
     <input type="search" id="search" name="search" placeholder="검색" />
-    <button id="addEmployeeButton" data-color="neutral" data-shape="block">직원 추가</button>
+    <button id="addEmployeeButton" data-color="positive" data-shape="block">직원 추가</button>
   </div>
 
-  <div>
+
   <table class="${styles.table__container}">
         <thead class="${styles.table__head}">
-          <tr class="${styles.userlist}">
-            <th class="${styles.profileimg}">사진</th>
-            <th class="${styles.name}">이름</th>
-            <th class="${styles.position}">직함</th>
-            <th class="${styles.email}">이메일</th>
-            <th class="${styles.birthday}">생일</th>
-            <th class="${styles.joinday}">입사일</th>
-            <th class="${styles.phonenumber}">핸드폰 번호</th>
-            <th class="${styles.button}">관리</th>
+          <tr class="${styles.head__userlist}">
+            <th class="${styles.head__profileimg}">사진</th>
+            <th class="${styles.head__name}">이름</th>
+            <th class="${styles.head__position}">직함</th>
+            <th class="${styles.head__email}">이메일</th>
+            <th class="${styles.head__birthday}">생일</th>
+            <th class="${styles.head__joinday}">입사일</th>
+            <th class="${styles.head__phonenumber}">핸드폰 번호</th>
+            <th class="${styles.head__button}">관리</th>
           </tr>
         </thead>
-        <tbody class="${styles.table__body} "id="employeeTableBody"></tbody>    
+        <tbody class="${styles.table__body}" id="employeeTableBody"></tbody>    
   </table>
-  </div>
 
   `;
 
@@ -46,8 +45,18 @@ const renderAdminEmployee = (container) => {
     });
   }
 
-  // JSON 데이터를 사용하여 테이블에 직원 데이터를 추가
-  loadEmployees(employeeData);
+  // 서버에서 JSON 데이터를 fetch하여 테이블에 직원 데이터를 추가
+  initializeEmployeeData();
+};
+
+// fetch 함수 호출 및 데이터 초기화 함수
+const initializeEmployeeData = async () => {
+  try {
+    const data = await fetchEmployeeData();
+    loadEmployees(data);
+  } catch (error) {
+    console.error("initializeEmployeeData 함수에서 오류 발생:", error);
+  }
 };
 
 // 테이블에 직원 데이터를 추가하는 함수
@@ -59,25 +68,23 @@ const loadEmployees = (employees) => {
     const row = document.createElement("tr");
 
     row.innerHTML = `
-      <tr class="${styles.table__tr}">
-      <td class="${styles.profileimg}"><img src="${employee.profileimg}" alt="profileimg" class="${styles.profileimg}"></td>
+      <td class="${styles.profileimg}"><img src="${employee.img}" alt="profileimg"></td>
       <td class="${styles.name}">${employee.name}</td>
       <td class="${styles.position}">${employee.position}</td>
       <td class="${styles.email}">${employee.email}</td>
       <td class="${styles.birthday}">${employee.birthday}</td>
       <td class="${styles.joinday}">${employee.joinday}</td>
       <td class="${styles.phonenumber}">${employee.phone}</td>
-      <td class="${styles.button}"><button data-color='neutral' data-shape='line'">수정</button></td>
-      </tr>
+      <td class="${styles.button}"><button data-color='neutral' data-shape='line'>수정</button></td>
     `;
 
     tbody.appendChild(row);
 
     // '수정' 버튼 클릭 이벤트 핸들러 추가
-    const addEmployeeButton = document.getElementById("addEmployeeButton");
-    if (addEmployeeButton) {
-      addEmployeeButton.addEventListener("click", () => {
-        renderAddEmployeePage(container); // 직원 수정 페이지로 이동
+    const editButton = row.querySelector(`.${styles.button} button`);
+    if (editButton) {
+      editButton.addEventListener("click", () => {
+        renderEditEmployeePage(container); // 직원 수정 페이지로 이동
       });
     }
   });

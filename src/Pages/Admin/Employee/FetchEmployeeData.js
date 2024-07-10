8e3cefca4eed4588e68dd1c4210e882d04dd3767
Suhyun Park <suhyun_4951@naver.com>
2024-07-11 02:13:05 +0900

    feat: 로딩스피너 코드 추가

--- src/Pages/Admin/Employee/FetchEmployeeData.js
+++ src/Pages/Admin/Employee/FetchEmployeeData.js
@@ -1,7 +1,12 @@
 import filter from "./Filter";
+import spinner from "/src/Components/Spinner/Spinner";
+
+// 로딩 스피너
+const loadingSpinner = spinner();
 
 const fetchEmployeeData = async () => {
   try {
+    loadingSpinner.show();
     const response = await fetch("/api/users");
 
     if (!response.ok) {
@@ -18,6 +23,8 @@ const fetchEmployeeData = async () => {
     }
   } catch (error) {
     console.error("Failed to fetch notices:", error);
+  } finally {
+    loadingSpinner.hide();
   }
   return []; // 에러 발생 시 빈 배열 반환
 };

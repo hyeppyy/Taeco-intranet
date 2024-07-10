c2dafc1bb3636ea3a8bc3c334340f2e7c4c5b4fc
Suhyun Park <suhyun_4951@naver.com>
2024-07-10 15:19:05 +0900

    feat: 오늘의 입사자/생일자 기능 구현

--- src/Pages/Admin/Employee/Employee.js
+++ src/Pages/Admin/Employee/Employee.js
@@ -9,11 +9,11 @@ const renderAdminEmployee = (container) => {
   <div class="${styles.page__event}">
     <div class="${styles.birthdayinfo}">
     <img src="/public/icons/cake.svg" alt="cake" />
-    <span class="${styles.event__birthday}"><h3>금일의 생일자: 홍길동</h3></span>
+    <h3><span class="${styles.event__birthday}" id="todayBirthday">금일의 생일자: 로딩 중 ...</span></h3>
     </div>
     <div class="${styles.joindayinfo}">
     <img src="/public/icons/seedling.svg" alt="seedling" />
-    <span class="${styles.event__joinday}"><h3>금일의 입사자: 홍길동</h3></span>
+    <h3><span class="${styles.event__joinday}" id="todayJoinDay">금일의 입사자: 로딩 중 ...</span></h3>
     </div>
   </div>
 
@@ -49,17 +49,56 @@ const renderAdminEmployee = (container) => {
   fetchAndDisplayEmployeeData();
 };
 
+// 총인원 기능 구현
 const fetchAndDisplayEmployeeData = async () => {
   const data = await fetchEmployeeData();
   if (data && data.length) {
     document.getElementById(
       "totalEmployeeCount"
     ).innerText = `총 인원: ${data.length}명`;
+    displayTodayEvents(data);
   }
 };
 
+// 오늘의 입사자/생일자 기능 구현
+const displayTodayEvents = (data) => {
+  const today = new Date();
+  const todayMonth = (today.getMonth() + 1).toString().padStart(2, "0");
+  const todayDate = today.getDate().toString().padStart(2, "0");
+
+  // console.log(`Today: ${todayMonth}.${todayDate}`); // 디버깅용
+
+  const birthdayPeople = data.filter((employee) => {
+    const [year, month, day] = employee.birthday.split(/[-.]/);
+    const isBirthday = month === todayMonth && day === todayDate;
+    // console.log(
+    //   `Checking birthday for ${employee.name}: ${employee.birthday} -> ${isBirthday}`
+    // ); // 디버깅용
+    return isBirthday;
+  });
+
+  const joinDayPeople = data.filter((employee) => {
+    const [year, month, day] = employee.startDate.split(/[-.]/);
+    const isJoinDay = month === todayMonth && day === todayDate;
+    // console.log(
+    //   `Checking join day for ${employee.name}: ${employee.startDate} -> ${isJoinDay}`
+    // ); // 디버깅용
+    return isJoinDay;
+  });
+
+  const birthdayNames = birthdayPeople.map((person) => person.name).join(", ");
+  const joinDayNames = joinDayPeople.map((person) => person.name).join(", ");
+
+  document.getElementById("todayBirthday").innerText = `금일의 생일자: ${
+    birthdayNames || "없음"
+  }`;
+  document.getElementById("todayJoinDay").innerText = `금일의 입사자: ${
+    joinDayNames || "없음"
+  }`;
+};
+
+// '직원 추가' 버튼 클릭 이벤트 핸들러 추가
 const addEmployee = () => {
-  // '직원 추가' 버튼 클릭 이벤트 핸들러 추가
   const addEmployeeButton = document.getElementById("addEmployeeButton");
   if (addEmployeeButton) {
     addEmployeeButton.addEventListener("click", () => {

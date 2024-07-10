57dd8208ac9db9b9119761d6f273332a02431536
Suhyun Park <suhyun_4951@naver.com>
2024-07-11 00:15:32 +0900

    design: 직원 정보 input -> div 변경

--- src/Pages/Admin/Employee/EditEmployee.js
+++ src/Pages/Admin/Employee/EditEmployee.js
@@ -21,10 +21,10 @@ const fillEditForm = () => {
   const employeeData = getQueryParams();
 
   if (employeeData.id) {
-    document.getElementById("name").value = employeeData.name;
-    document.getElementById("positions").value = employeeData.position;
-    document.getElementById("email").value = employeeData.email;
-    document.getElementById("phone").value = employeeData.phone;
+    document.getElementById("name").textContent = employeeData.name;
+    document.getElementById("positions").textContent = employeeData.position;
+    document.getElementById("email").textContent = employeeData.email;
+    document.getElementById("phone").textContent = employeeData.phone;
     // 날짜 형식 설정
     const formatDate = (dateString) => {
       const date = new Date(dateString);
@@ -34,10 +34,10 @@ const fillEditForm = () => {
       return `${yyyy}-${mm}-${dd}`;
     };
 
-    document.getElementById("birthday").value = formatDate(
+    document.getElementById("birthday").textContent = formatDate(
       employeeData.birthday
     );
-    document.getElementById("joinday").value = formatDate(
+    document.getElementById("joinday").textContent = formatDate(
       employeeData.startDate
     );
     document.querySelector('img[alt="profileimg"]').src =
@@ -61,51 +61,51 @@ const renderEditEmployeePage = (container) => {
     <img src="/public/images/_Avatar_.png" alt="profileimg">
   </div>
 
-  <form class="infoform"action="#" method="post">
+  <form class="infoform" action="#" method="post">
             <ul class="${styles.user__info}">
               <li class="${styles.info__name}">
                 <label for="name"><h5>이름</h5></label>
-                <input data-shape="line" type="name" id="name" placeholder="홍길동" required/>
+                <div id="name" class="${styles.info__value}"></div>
               </li>
 
               <li class="${styles.info__position}">
                 <label for="positions"><h5>직함</h5></label>
-                <select name="positions" id="positions" required>
-                  <option value="사장">사장</option>
-                  <option value="부장">부장</option>
-                  <option value="차장">차장</option>
-                  <option value="과장">과장</option>
-                  <option value="대리">대리</option>
-                  <option value="사원">사원</option>
-                </select>
+                <div id="positions" class="${styles.info__value}"></div>
               </li>
 
               <li class="${styles.info__email}">
                 <label for="email"><h5>이메일</h5></label>
-                <input data-shape="line" type="email" id="email" placeholder="eco@taeco.com" required/>
+                <div id="email" class="${styles.info__value}"></div>
               </li>
 
               <li class="${styles.info__phonenumber}">
                 <label for="phone"><h5>전화번호</h5></label>
-                <input data-shape="line" type="number" id="phone" placeholder="010-0000-0000" required/>
+                <div id="phone" class="${styles.info__value}"></div>
               </li>
 
               <li class="${styles.info__birthday}">
                 <label for="birthday"><h5>생일</h5></label>
-                <input data-shape="line" type="date" id="birthday" required/>
+                <div id="birthday" class="${styles.info__value}"></div>
               </li>
               
               <li class="${styles.info__joinday}">
                 <label for="joinday"><h5>입사일</h5></label>
-                <input data-shape="line" type="date" id="joinday" required/>
+                <div id="joinday" class="${styles.info__value}"></div>
               </li>
             </ul>
-          </form>
+</form>
   `;
 
   document.getElementById("backButton").addEventListener("click", () => {
     history.pushState(null, null, "/admin/employee");
     route();
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(`.${styles.info__value}`);
+    elements.forEach((element) => {
+      element.textContent = element.getAttribute("data-value");
+    });
   });
 
   // 페이지 로드 시 폼 채우기

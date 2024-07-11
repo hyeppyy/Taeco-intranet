a6731b2938e8427c559b1aec8de1fe45d2205eb6
Mark Kwon <redhero8830@gmail.com>
2024-07-11 16:25:54 +0900

    fixed: 마일리지 히스토리 페이지 수정

--- src/Pages/User/Mileage/History/FetchMileageData.js
+++ src/Pages/User/Mileage/History/FetchMileageData.js
@@ -1,5 +1,6 @@
-import renderMileageList from './RenderMileageList';
-import initPagination from '/src/Components/Pagination/Pagination';
+import renderMileageList from "./RenderMileageList";
+import initPagination from "/src/Components/Pagination/Pagination";
+import styles from "./History.module.css";
 
 // 서버 데이터 요청 함수
 const fetchMileageData = async () => {
@@ -7,19 +8,22 @@ const fetchMileageData = async () => {
   // const data = await response.json();
   // initPagination(data, renderMileageList);
   try {
-    const response = await fetch('/api/mileage');
+    const response = await fetch("/api/mileage");
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
-    console.log(data.data);
-    if (data.status === 'OK') {
-      initPagination(data.data, renderMileageList);
+    const approvedMileage = data.data.filter(
+      (item) =>
+        item.user === sessionStorage.getItem("userName") && item.isApprove == 1
+    );
+    if (data.status === "OK") {
+      initPagination(approvedMileage, renderMileageList);
     } else {
-      console.error('Error fetching mileage data:', data.error);
+      console.error("Error fetching mileage data:", data.error);
     }
   } catch (error) {
-    console.error('Error fetching mileage data:', error);
+    console.error("Error fetching mileage data:", error);
   }
 };
 
